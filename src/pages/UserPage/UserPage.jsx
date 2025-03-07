80fdfa43ef42088f90e1c94045e58d4687845cfb
aliso4ka <gulyayevaalisa@gmail.com>
2025-03-07 21:02:58 +0100

    fixed

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -3,7 +3,7 @@ import GreenBlock from "../../components/GreenBlock/GreenBlock";
 import ProgressBar from "../../components/WaterProgressBar/WaterProgressBar.jsx";
 import UserGreyBlock from "../../components/UserGreyBlock/UserGreyBlock.jsx";
 import AddWaterBtn from "../../components/AddWaterBtn/AddWaterBtn.jsx";
-import DailyNorma from "../../DailyNorma/DailyNorma.jsx";
+import DailyNorma from "../../components/DailyNorma/DailyNorma.jsx";
 
 const UserPage = () => {
   return (
