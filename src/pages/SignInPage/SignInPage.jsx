d0b5ffefc15530c2570f03a1f70bb752a7fde13c
Oleh Shyian <shiyan4242@gmail.com>
2025-03-08 09:45:46 +0100

    fix

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import GreyBlock from "../../components/GreyBlock/GreyBlock";
 import ImgBlock from "../../components/ImgBlock/ImgBlock";
-import s from "./SignInPage.module.css";
 
 const SignInPage = () => {
   const [hideImgBlock, setHideImgBlock] = useState(false);
@@ -22,39 +21,6 @@ const SignInPage = () => {
   return (
     <>
       <GreyBlock>
-        <div className={s.signinContainer}>
-          <h2 className={s.signinTitle}>Sign In</h2>
-          <form className={s.signinForm}>
-            <div className={s.inputGroup}>
-              <label htmlFor="email" className={s.label}>Email</label>
-              <input
-                type="email"
-                id="email"
-                placeholder="Enter your email"
-                className={s.input}
-              />
-            </div>
-            <div className={s.inputGroup}>
-              <label htmlFor="password" className={s.label}>Password</label>
-              <input
-                type="password"
-                id="password"
-                placeholder="Enter your password"
-                className={s.input}
-              />
-            </div>
-            </form>
-            <button type="submit" className={s.signinButton}>
-              Sign In
-            </button>
-          
-          <p className={s.signupText}>
-            Don’t have an account?{" "}
-            <a href="#" className={s.signupLink}>
-              Sign Up
-            </a>
-          </p>
-        </div>
       </GreyBlock>
       {hideImgBlock && (
         <ImgBlock>
