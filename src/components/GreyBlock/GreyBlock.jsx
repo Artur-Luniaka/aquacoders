71afaf30b41e44f797bd5861ee6fde17b924e967
Oleh Shyian <shiyan4242@gmail.com>
2025-03-09 18:42:35 +0100

    fix

--- src/components/GreyBlock/GreyBlock.jsx
+++ src/components/GreyBlock/GreyBlock.jsx
@@ -1,11 +1,10 @@
 import s from "./GreyBlock.module.css";
-import SignInForm from "../SigninForm/SignInForm";
 
 const GreyBlock = ({ children }) => {
   return (
     <div className={s.box}>
       {children}
-      <SignInForm />
+      
     </div>
   );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import GreyBlock from "../../components/GreyBlock/GreyBlock";
 import ImgBlock from "../../components/ImgBlock/ImgBlock";
+import SignInForm from "../../components/SigninForm/SignInForm";
 
 const HomePage = () => {
   return (
     <>
       <GreyBlock>
         <h2>Тут будет текстовый контент страницы Home</h2>
+        <SignInForm />
       </GreyBlock>
       <ImgBlock>
         <h2>Тут будет контент на картинке</h2>
