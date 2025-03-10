d7b5b1c5a6b44b17521416e17fd72c65fbcfb15d
Oleh Shyian <shiyan4242@gmail.com>
2025-03-10 11:45:18 +0100

    btn

--- src/components/SigninForm/SignInForm.jsx
+++ src/components/SigninForm/SignInForm.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import * as Yup from "yup";
 import s from "./SignInForm.module.css";
 import { yupResolver } from "@hookform/resolvers/yup";
+import sprite from "../../assets/sprite.svg";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: Yup.string().required("Password is required"),
+});
 
 const SignInForm = () => {
-  const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .email("Invalid email format")
-      .required("Email is required"),
-    password: Yup.string().required("Password is required"),
-  });
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
@@ -19,6 +22,10 @@ const SignInForm = () => {
   } = useForm({
     resolver: yupResolver(validationSchema),
   });
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   const onSubmit = async (data) => {
     console.log(data);
@@ -39,30 +46,48 @@ const SignInForm = () => {
             className={errors.email ? `${s.input} ${s.inputError}` : s.input}
             {...register("email")}
           />
-          {errors.email && (
-            <p className={s.error_text}>{errors.email.message}</p>
+          {errors.email ? (
+            <span className={s.error_text}>{errors.email.message}</span>
+          ) : (
+            <span className={s.error_placeholder}></span>
           )}
         </div>
         <div className={s.input_group}>
           <label htmlFor="password" className={s.label}>
             Password
           </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter your password"
-            className={errors.password ? `${s.input} ${s.inputError}` : s.input}
-            {...register("password")}
-          />
-          {errors.password && (
-            <p className={s.error_text}>{errors.password.message}</p>
+          <div className={s.password_wrapper}>
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              placeholder="Enter your password"
+              className={errors.password ? `${s.input} ${s.inputError}` : s.input}
+              {...register("password")}
+            />
+            <button
+              className={s.icon_btn}
+              onClick={togglePasswordVisibility}
+              type="button"
+            >
+              <svg className={s.icon}>
+                <use
+                  href={`${sprite}#${
+                    showPassword ? "icon-eye" : "icon-eye-off"
+                  }`}
+                />
+              </svg>
+            </button>
+          </div>
+          {errors.password ? (
+            <span className={s.error_text}>{errors.password.message}</span>
+          ) : (
+            <span className={s.error_placeholder}></span>
           )}
         </div>
-        
         <button type="submit" className={s.signin_button}>
           Sign In
         </button>
-        </form>
+      </form>
       <p className={s.signup_text}>
         Don’t have an account?{" "}
         <a href="/signup" className={s.signup_link}>
@@ -74,3 +99,4 @@ const SignInForm = () => {
 };
 
 export default SignInForm;
+
