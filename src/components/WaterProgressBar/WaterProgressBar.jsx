155c5978881f15f6dd9ba3b85523761fbe359272
ftxprox <ftxprox@gmail.com>
2025-03-10 19:08:07 +0200

    super_mega_fix LogOutModal,DeleteEntryModal,AdvantageSection,WaterProgressBar

--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -4,13 +4,12 @@ const WaterProgressBar = () => {
 
 return (
     <div className={s.container}>
-      
       <p className={s.title}>Today</p>
       <div className={s.slider}><div className={s.rail}></div></div>
         <div className={s.percent_bar}>
-            <p>0%</p>
-            <p className={s.fifty}>50%</p>
-            <p>100%</p>
+            <span>0%</span>
+            <span className={s.fifty}>50%</span>
+            <span>100%</span>
         </div>
     </div>
 
