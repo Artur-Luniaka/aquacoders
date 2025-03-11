cc1f1f28c773c9f069651d2b7181210c72581576
Artur Luniaka <artur01049718@gmail.com>
2025-03-11 10:17:03 +0100

    set some moments

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,6 @@ import MainImg from "../../components/MainImg/MainImg";
 import AdvantagesSection from "../../components/AdvantegesSection/AdvantagesSection.jsx";
 import WelcomeSection from "../../components/WelcomeSection/WelcomeSection.jsx";
 import OurHappyCustomers from "../../components/OurHappyCustomers/OurHappyCustomers.jsx";
-import SettingsModal from "../../components/SettingsModal/SettingsModal.jsx";
 
 const HomePage = () => {
   return (
@@ -19,9 +18,6 @@ const HomePage = () => {
         <AdvantagesSection />
         <OurHappyCustomers />
       </ImgBlock>
-      <GreyBlock>
-        <SettingsModal />
-      </GreyBlock>
     </>
   );
 };
