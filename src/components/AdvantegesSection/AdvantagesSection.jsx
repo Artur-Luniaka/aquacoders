bcf9a36d8045d9756851d796b8606887b46210ea
ftxprox <ftxprox@gmail.com>
2025-03-11 09:16:17 +0200

    fix percent bar and modal

--- src/components/AdvantegesSection/AdvantagesSection.jsx
+++ src/components/AdvantegesSection/AdvantagesSection.jsx
@@ -1,11 +1,7 @@
 import s from "./AdvantagesSection.module.css";
-import LogOutModal from "..//LogOutModal/LogOutModal.jsx"
-
 
 const AdvantagesSection = () => {
   return (
-    <>
-    <LogOutModal/>
     <div className={s.info_container}>
       <ul className={s.info_list}>
         <li className={s.info_item}>
@@ -19,7 +15,6 @@ const AdvantagesSection = () => {
       </li>
       </ul>
     </div>
-    </>
   );
  
 };
